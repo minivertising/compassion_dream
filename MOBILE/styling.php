21e8ec4c1cadc3fa293e5067897d51717e2f1ac5
ojoonwoo <jw.o@minivertising.kr>
2016-05-20 18:01:23 +0900

    crop image download

--- MOBILE/styling.php
+++ MOBILE/styling.php
@@ -10,10 +10,9 @@
   <script src="../lib/Cropper/js/jquery.min.js"></script>
   <script src="../lib/Cropper/js/bootstrap.min.js"></script>
   <script src="../lib/Cropper/js/cropper.js"></script>
-  <script src="../lib/Cropper/js/main.js"></script>
   <style>
     .container {
-      max-width: 960px;
+      max-width: 100%;
     }
 
     img {
@@ -27,49 +26,55 @@
     <div>
       <img id="ori_image" src="./images/picture.jpg" alt="Picture">
     </div>
-    <div class="btn-group btn-group-crop">
-    <form>
+    <div class="btn-group btn-group-crop docs-buttons">
+    <!-- <form> -->
           <!--<button type="button" class="btn btn-primary" data-method="getCroppedCanvas">-->
-          <button type="button" class="btn btn-primary" data-method="getCroppedCanvas">
-    </form>
+          <!-- <button type="button" class="btn btn-primary" id="download" data-method="getCroppedCanvas" href="javascript:void(0);" download="cropped.jpg"> -->
+          <a class="btn btn-primary" id="download" href="javascript:void(0);" data-method="getCroppedCanvas" download="cropped.jpg">Download</a>
+    <!-- </form> -->
     </div>
   </div>
   <script>
     $(function () {
-      $('#ori_image').cropper({
-        viewMode: 1,
-        dragMode: 'move',
-        autoCropArea: 0.65,
-        restore: false,
-        guides: false,
-        highlight: false,
-        cropBoxMovable: false,
-        cropBoxResizable: false
-      });
+      // $(document).ready(function(){
+        $('#ori_image').cropper({
+          viewMode: 1,
+          dragMode: 'move',
+          autoCropArea: 0.65,
+          restore: false,
+          guides: false,
+          highlight: false,
+          cropBoxMovable: false,
+          cropBoxResizable: false,
+          minCropBoxWidth: 300,
+          minCropBoxHeight: 150
+        });
+     // })
     });
 
-    function down_crop_image()
-    {
-      // Upload cropped image to server if the browser supports `HTMLCanvasElement.toBlob`
-      $('#ori_image').cropper('getCroppedCanvas').toBlob(function (blob) {
-        var formData = new FormData();
+    // function down_crop_image()
+    // {
+    //   // Upload cropped image to server if the browser supports `HTMLCanvasElement.toBlob`
+    //   $('#ori_image').cropper('getCroppedCanvas').toBlob(function (blob) {
+    //     var formData = new FormData();
 
-        formData.append('croppedImage', blob);
+    //     formData.append('croppedImage', blob);
 
-        $.ajax('./images/upload', {
-          method: "POST",
-          data: formData,
-          processData: false,
-          contentType: false,
-          success: function () {
-            console.log('Upload success');
-          },
-          error: function () {
-            console.log('Upload error');
-          }
-        });
-      });      
-    }
+    //     $.ajax('./images/upload', {
+    //       method: "POST",
+    //       data: formData,
+    //       processData: false,
+    //       contentType: false,
+    //       success: function () {
+    //         console.log('Upload success');
+    //       },
+    //       error: function () {
+    //         console.log('Upload error');
+    //       }
+    //     });
+    //   });      
+    // }
   </script>
+  <script src="../lib/Cropper/js/main.js"></script>
 </body>
 </html>
