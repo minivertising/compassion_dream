7255a4e6e2da14114fe9d9d39169081246352a69
kyh <kyhfan@gmail.com>
2016-05-30 14:56:53 +0900

    111

--- js/m_main.js
+++ js/m_main.js
@@ -1,7 +1,7 @@
 function open_pop(param)
 {
 	// alert("op_pp");
-	$.colorbox({innerWidth:"100%",innerHeight: "70%", initialWidth:"95%", initialHeight: "70%", inline:true, opacity:"0.9", scrolling:false, reposition: false,closeButton:false, overlayClose: false, open:true, speed:0, fadeOut: 300, href:"#"+param, onComplete: function(){
+	$.colorbox({innerWidth:"100%",innerHeight: "70%", initialWidth:"95%", initialHeight: "70%", inline:true, opacity:"0.9", scrolling:true, reposition: false,closeButton:false, overlayClose: false, open:true, speed:0, fadeOut: 300, href:"#"+param, onComplete: function(){
 		$("#cboxContent").css("background","none");
 		$("#cboxContent").css("z-index","99999");
 		$('#cboxWrapper').css('backgroundColor', "");
