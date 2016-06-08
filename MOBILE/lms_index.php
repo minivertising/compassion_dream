8fd6cc517e08e0a2e0b94f8d322d08dee8a3e42a
kyh <kyhfan@gmail.com>
2016-06-08 11:11:20 +0900

    페이스북 app id 추가

--- MOBILE/lms_index.php
+++ MOBILE/lms_index.php
@@ -4,6 +4,23 @@ $rs	= $_REQUEST['rs'];
 print_r($rs);
 ?>
 <body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '649187078561789',
+      xfbml      : true,
+      version    : 'v2.6'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 	<div id="loading_div" style="display:none">
 		Loading.... 꿈이 필요한 아이와 매칭중
 	</div>
