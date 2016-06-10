83f00bb225695ed0191bee873dbab346f28218c3
ojoonwoo <jw.o@minivertising.kr>
2016-06-10 10:28:19 +0900

    sel_dream check

--- MOBILE/lms_index.php
+++ MOBILE/lms_index.php
@@ -1,5 +1,5 @@
 <?
-	include_once "./header.php";
+    include_once "./header.php";
 ?>
 <body>
 <script>
@@ -19,257 +19,262 @@
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
 </script>
-	<div id="loading_div" style="display:none">
-		Loading.... 꿈이 필요한 아이와 매칭중
-	</div>
-	<div id="contents_div">
-		<a href="#" onclick="open_pop('dream_sel_popup');">지금 참여하기</a>
-		<?
-		include_once "./popup_div.php";
-		?>
-	</div>
+    <div id="loading_div" style="display:none">
+        Loading.... 꿈이 필요한 아이와 매칭중
+    </div>
+    <div id="contents_div">
+        <a href="#" onclick="open_pop('dream_sel_popup');">지금 참여하기</a>
+        <?
+        include_once "./popup_div.php";
+        ?>
+    </div>
 </body>
 </html>
 <script type="text/javascript">
-	var sel_dream		= null;
-	var runner_serial	= null;
-	var mb_job			= null;
-	var mb_image		= null;
-	var $ori_image = $('#ori_image');
-	var $previews = $('.preview');
-	var imageData;
-	var afterCropBoxData;
-	var ratioWidth;
-	var ratioHeight;
-	var destCropWidth;
-	var destCropHeight;
-	var centerCropBoxWidth;
-	var centerCropBoxHeight;
-	var flag_sel_dream	= 0;
-	var mb_rs		= '<?=$rs?>';
-	$(document).ready(function() {
-		Kakao.init('59df63251be6d99256b63b98f4948e89');
-		$("#cboxTopLeft").hide();
-		$("#cboxTopRight").hide();
-		$("#cboxBottomLeft").hide();
-		$("#cboxBottomRight").hide();
-		$("#cboxMiddleLeft").hide();
-		$("#cboxMiddleRight").hide();
-		$("#cboxTopCenter").hide();
-		$("#cboxBottomCenter").hide();
-	});
-	var $inputImage = $('#inputImage');
-	var URL = window.URL || window.webkitURL;
-	var blobURL;
+    var sel_dream       = null;
+    var runner_serial   = null;
+    var mb_job          = null;
+    var mb_image        = null;
+    var $ori_image = $('#ori_image');
+    var $previews = $('.preview');
+    var imageData;
+    var afterCropBoxData;
+    var ratioWidth;
+    var ratioHeight;
+    var destCropWidth;
+    var destCropHeight;
+    var centerCropBoxWidth;
+    var centerCropBoxHeight;
+    var flag_sel_dream  = 0;
+    var mb_rs       = '<?=$rs?>';
+    $(document).ready(function() {
+        Kakao.init('59df63251be6d99256b63b98f4948e89');
+        $("#cboxTopLeft").hide();
+        $("#cboxTopRight").hide();
+        $("#cboxBottomLeft").hide();
+        $("#cboxBottomRight").hide();
+        $("#cboxMiddleLeft").hide();
+        $("#cboxMiddleRight").hide();
+        $("#cboxTopCenter").hide();
+        $("#cboxBottomCenter").hide();
+    });
+    var $inputImage = $('#inputImage');
+    var URL = window.URL || window.webkitURL;
+    var blobURL;
 
-	  if (URL) {
-	    $inputImage.change(function () {
-	      var files = this.files;
-	      var file;
+      if (URL) {
+        $inputImage.change(function () {
+          var files = this.files;
+          var file;
 
-	      if (!$ori_image.data('cropper')) {
-	        return;
-	      }
+          if (!$ori_image.data('cropper')) {
+            return;
+          }
 
-	      if (files && files.length) {
-	        file = files[0];
+          if (files && files.length) {
+            file = files[0];
 
-	        if (/^image\/\w+$/.test(file.type)) {
-	          blobURL = URL.createObjectURL(file);
-	          $ori_image.one('built.cropper', function () {
-	            // Revoke when load complete
-	            URL.revokeObjectURL(blobURL);
-	          }).cropper('reset').cropper('replace', blobURL);
-	          $inputImage.val('');
-	        } else {
-	          window.alert('Please choose an image file.');
-	        }
-	      }
-	    });
-	  } else {
-	    $inputImage.prop('disabled', true).parent().addClass('disabled');
-	  }
+            if (/^image\/\w+$/.test(file.type)) {
+              blobURL = URL.createObjectURL(file);
+              $ori_image.one('built.cropper', function () {
+                // Revoke when load complete
+                URL.revokeObjectURL(blobURL);
+              }).cropper('reset').cropper('replace', blobURL);
+              $inputImage.val('');
+            } else {
+              window.alert('Please choose an image file.');
+            }
+          }
+        });
+      } else {
+        $inputImage.prop('disabled', true).parent().addClass('disabled');
+      }
 
 
 // $(function () {
-	
+    
 
 // });
 
 function image_crop(){
-	$($ori_image).cropper({
-		viewMode: 0,
-		dragMode: 'move',
-		autoCropArea: 0.8,
-		aspectRatio: 1200/630,
-		responsive: false,
-		restore: false,
-		guides: false,
-		highlight: false,
-		cropBoxMovable: false,
-		cropBoxResizable: false,
-		preview: '.preview',
-		center:true,
-			// minCropBoxWidth:1200,
-			// minCropBoxHeight:630,
-			// built: function(){
-				// alert("built");
-				// $($ori_image).cropper('setCanvasData', {
-				// 	left:0,
-				// 	top:0,
-				// 	width: 300,
-				// 	height: 200
-				// });
-				// $($ori_image).cropper('setCropBoxData', {
-				// 	left:0,
-				// 	top:0,
-				// 	width: 200,
-				// 	height: 100
-				// });
-			// 	imageData = $($ori_image).cropper('getImageData');
-			// 	afterCropBoxData = $($ori_image).cropper('getCropBoxData');
-			// 	// console.log("naturalWidth: "+imageData.naturalWidth+",  naturalHeight: "+imageData.naturalHeight); // 오리지널 사이즈
-			// 	// console.log("afterImageWidth: "+imageData.width+",  afterImageHeight: "+imageData.height);
-			// 	// console.log("afterCropBoxWidth: "+afterCropBoxData.width+",  afterCropBoxHeight: "+afterCropBoxData.height);
-			// 	ratioWidth = imageData.width/imageData.naturalWidth;
-			// 	if(imageData.naturalHeight < 630) {
-			// 		ratioHeight = 1;
-			// 	}else{
-			// 		ratioHeight = imageData.height/imageData.naturalHeight;
-			// 	}
-			// 	destCropWidth = (1200*ratioWidth); 
-			// 	destCropHeight = (630*ratioHeight);
-			// 	centerCropBoxWidth = (imageData.width-destCropWidth)/2;
-			// 	centerCropBoxHeight = (imageData.height-destCropHeight)/2;
-			// 	$($ori_image).cropper("setCropBoxData", {left: centerCropBoxWidth, top: centerCropBoxHeight, width: destCropWidth, height: destCropHeight});
-		// },
-		build: function (e) {
-      	  console.log(e.type);
-	    },
-	    built: function (e) {
-	      console.log(e.type);
-	    },
-	    cropstart: function (e) {
-	      console.log(e.type, e.action);
-	    },
-	    cropper: function (e) {
-	      console.log(e.type, e.action);
-	    },
-	    cropend: function (e) {
-	      console.log(e.type, e.action);
-	    },
-	    crop: function (e) {
-	      console.log(e.type, e.x, e.y, e.width, e.height, e.rotate, e.scaleX, e.scaleY);
-	    },
-	    zoom: function (e) {
-	      console.log(e.type, e.ratio);
-   		 }
-	});
+    $($ori_image).cropper({
+        viewMode: 0,
+        dragMode: 'move',
+        autoCropArea: 0.8,
+        aspectRatio: 1200/630,
+        responsive: false,
+        restore: false,
+        guides: false,
+        highlight: false,
+        cropBoxMovable: false,
+        cropBoxResizable: false,
+        preview: '.preview',
+        center:true,
+            // minCropBoxWidth:1200,
+            // minCropBoxHeight:630,
+            // built: function(){
+                // alert("built");
+                // $($ori_image).cropper('setCanvasData', {
+                //  left:0,
+                //  top:0,
+                //  width: 300,
+                //  height: 200
+                // });
+                // $($ori_image).cropper('setCropBoxData', {
+                //  left:0,
+                //  top:0,
+                //  width: 200,
+                //  height: 100
+                // });
+            //  imageData = $($ori_image).cropper('getImageData');
+            //  afterCropBoxData = $($ori_image).cropper('getCropBoxData');
+            //  // console.log("naturalWidth: "+imageData.naturalWidth+",  naturalHeight: "+imageData.naturalHeight); // 오리지널 사이즈
+            //  // console.log("afterImageWidth: "+imageData.width+",  afterImageHeight: "+imageData.height);
+            //  // console.log("afterCropBoxWidth: "+afterCropBoxData.width+",  afterCropBoxHeight: "+afterCropBoxData.height);
+            //  ratioWidth = imageData.width/imageData.naturalWidth;
+            //  if(imageData.naturalHeight < 630) {
+            //      ratioHeight = 1;
+            //  }else{
+            //      ratioHeight = imageData.height/imageData.naturalHeight;
+            //  }
+            //  destCropWidth = (1200*ratioWidth); 
+            //  destCropHeight = (630*ratioHeight);
+            //  centerCropBoxWidth = (imageData.width-destCropWidth)/2;
+            //  centerCropBoxHeight = (imageData.height-destCropHeight)/2;
+            //  $($ori_image).cropper("setCropBoxData", {left: centerCropBoxWidth, top: centerCropBoxHeight, width: destCropWidth, height: destCropHeight});
+        // },
+        build: function (e) {
+          console.log(e.type);
+        },
+        built: function (e) {
+          console.log(e.type);
+        },
+        cropstart: function (e) {
+          console.log(e.type, e.action);
+        },
+        cropper: function (e) {
+          console.log(e.type, e.action);
+        },
+        cropend: function (e) {
+          console.log(e.type, e.action);
+        },
+        crop: function (e) {
+          console.log(e.type, e.x, e.y, e.width, e.height, e.rotate, e.scaleX, e.scaleY);
+        },
+        zoom: function (e) {
+          console.log(e.type, e.ratio);
+         }
+    });
 }
 // });
 
 function preview_img()
 {
 /*
-	사진 저장할 내용 추가
+    사진 저장할 내용 추가
 */
-	open_pop('preview_popup');
+    open_pop('preview_popup');
 
 }
 
 function dream_next()
 {
-	//mb_job	= $("#mb_job").val();
+    if (sel_dream == null)
+        {
+            alert("당신의 어린시절 꿈을 선택해 주세요.");
+            return false;
+        }
+    //mb_job    = $("#mb_job").val();
 
-	// 사진 저장할 내용 추가
-	$($ori_image).cropper('getCroppedCanvas', {width:1200, height:630}).toBlob(function (blob) {
-		var formData = new FormData();
-	  // formData.append('croppedImage', blob);
-	  formData.append('croppedImage', blob, "test.jpg");
-	  $.ajax('./upload2.php', {
-	  	method: "POST",
-	  	data: formData,
-	  	processData: false,
-	  	contentType: false,
-	  	success: function (data) {
-	  		console.log(data);
-			// mb_image	= data;
-			// open_pop('input_popup');
-	  	}
-	  });
-	});
-	//    
-	if (mb_job == "")
-	{
-		alert("당신의 어린시절 꿈을 선택해 주세요.");
-		return false;
-	}
-	
+    // 사진 저장할 내용 추가
+    $($ori_image).cropper('getCroppedCanvas', {width:1200, height:630}).toBlob(function (blob) {
+      var formData = new FormData();
+      // formData.append('croppedImage', blob);
+      formData.append('croppedImage', blob, "test.jpg");
+      $.ajax('./upload2.php', {
+        method: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            // console.log(data);
+            mb_image    = data;
+            open_pop('input_popup');
+        }
+      });
+    });
+    //    
+    // if (mb_job == "")
+    // {
+    //     alert("당신의 어린시절 꿈을 선택해 주세요.");
+    //     return false;
+    // }
+    
 
 }
 
 function input_submit()
 {
-	var mb_name	= $("#mb_name").val();
-	var mb_phone	= $("#mb_phone").val();
-	//mb_image		= "임시 이미지 URL"; // 이미지 경로 작업 완료되면 여기에 값 추가
+    var mb_name = $("#mb_name").val();
+    var mb_phone    = $("#mb_phone").val();
+    //mb_image      = "임시 이미지 URL"; // 이미지 경로 작업 완료되면 여기에 값 추가
 
-	if (mb_name == "")
-	{
-		alert("이름을 입력해 주세요.");
-		return false;
-	}
+    if (mb_name == "")
+    {
+        alert("이름을 입력해 주세요.");
+        return false;
+    }
 
-	if (mb_phone == "")
-	{
-		alert("전화번호를 입력해 주세요.");
-		return false;
-	}
+    if (mb_phone == "")
+    {
+        alert("전화번호를 입력해 주세요.");
+        return false;
+    }
 
-	$.ajax({
-		type:"POST",
-		data:{
-			"exec"			: "insert_info",
-			"mb_name"		: mb_name,
-			"mb_phone"		: mb_phone,
-			"mb_job"			: sel_dream,
-			"mb_image"		: mb_image,
-			"mb_serial"		: mb_rs
-		},
-		url: "../main_exec.php",
-		beforeSend: function(response){
-			$("#loading_div").show();
-			$("#contents_div").hide();
-		},
-		success: function(response){
-			alert(response);
-			$("#loading_div").hide();
-			$("#contents_div").show();
-			if (response == "Y")
-			{
-				open_pop('share_popup');
-			}else {
-				alert("참여자가 많아 처리가 지연되고 있습니다. 다시 참여해 주세요.");
-				location.reload();
-			}
-		}
-	});
+    $.ajax({
+        type:"POST",
+        data:{
+            "exec"          : "insert_info",
+            "mb_name"       : mb_name,
+            "mb_phone"      : mb_phone,
+            "mb_job"        : sel_dream,
+            "mb_image"      : mb_image,
+            "mb_serial"     : mb_rs
+        },
+        url: "../main_exec.php",
+        beforeSend: function(response){
+            $("#loading_div").show();
+            $("#contents_div").hide();
+        },
+        success: function(response){
+            alert(response);
+            $("#loading_div").hide();
+            $("#contents_div").show();
+            if (response == "Y")
+            {
+                open_pop('share_popup');
+            }else {
+                alert("참여자가 많아 처리가 지연되고 있습니다. 다시 참여해 주세요.");
+                location.reload();
+            }
+        }
+    });
 }
 /*
 function readURL(input) {
-	console.log(input);
-	if (input.files && input.files[0]) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			$('#uploadImg').attr('src', e.target.result);
-		}
-		reader.readAsDataURL(input.files[0]);
-	}
+    console.log(input);
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#uploadImg').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
 }
 
 function img_submit()
 {
-	$("#upimage_frm").submit();
+    $("#upimage_frm").submit();
 }
 */
 
--- MOBILE/upload2.php
+++ MOBILE/upload2.php
@@ -6,32 +6,47 @@
 	$file_size = $_FILES['croppedImage']['size'];                 // 업로드한 파일의 크기
 	$mimeType = $_FILES['croppedImage']['type'];                 // 업로드한 파일의 MIME Type
 
-	if($mimeType=="html" || 
-		$mimeType=="htm" || 
-		$mimeType=="php" || 
-		$mimeType=="php3" || 
-		$mimeType=="inc" || 
-		$mimeType=="pl" || 
-		$mimeType=="cgi" || 
-		$mimeType=="txt" || 
-		$mimeType=="TXT" || 
-		$mimeType=="asp" || 
-		$mimeType=="jsp" || 
-		$mimeType=="phtml" || 
-		$mimeType=="js" || 
-		$mimeType=="") 
-	{ 
+
+	if($mimeType=="html" ||
+		$mimeType=="htm" ||
+		$mimeType=="php" ||
+		$mimeType=="php3" ||
+		$mimeType=="inc" ||
+		$mimeType=="pl" ||
+		$mimeType=="cgi" ||
+		$mimeType=="txt" ||
+		$mimeType=="TXT" ||
+		$mimeType=="asp" ||
+		$mimeType=="jsp" ||
+		$mimeType=="phtml" ||
+		$mimeType=="js" ||
+		$mimeType=="")
+	{
 		echo "X";
 		exit;
 	}
 
-	switch ($mimeType) {
-		case 'image/png':
-		$file_exe = "png";
-		break;
-		default:
-		$file_exe = "jpg";
-		break;
+	// switch ($mimeType) {
+	// 	case 'image/png':
+	// 	$file_exe = "png";
+	// 	break;
+	// 	default:
+	// 	$file_exe = "jpg";
+	// 	break;
+	// }
+
+	$real_name = $file_name;
+	$arr = explode(".", $real_name);	 // 원래 파일의 확장자명을 가져와서 그대로 적용 $file_exe	
+	$arr1 = $arr[0];
+	$arr2 = $arr[1];
+	$arr3 = $arr[2];
+	$arr4 = $arr[3];
+	if($arr4) {
+		$file_exe = $arr4;
+	} else if($arr3 && !$arr4) {
+		$file_exe = $arr3;
+	} else if($arr2 && !$arr3) {
+		$file_exe = $arr2;
 	}
 
 	$imageSize = getimagesize($file_tmp_name);
@@ -41,30 +56,36 @@
 	$save_path = '../files/'.date("Ymd").'/';
 
 	if ( !is_dir($save_path) ) {
-		if(@mkdir($save_path, 0777)) { 
-			if(is_dir($save_path)) { 
-				@chmod($save_path, 0777); 
+		if(@mkdir($save_path, 0777)) {
+			if(is_dir($save_path)) {
+				@chmod($save_path, 0777);
 			}
 		}
 	}
 
 	$save_full_path = $save_path.$save_name;
 
-	$result = move_uploaded_file($file_tmp_name, $save_full_path);
+	if(!move_uploaded_file($file_tmp_name, $save_full_path)){
+		die("파일을 지정한 디렉토리에 업로드하는데 실패했습니다.");
+	}else{
+		$targ_w = $imageSize[0];
+		$targ_h = $imageSize[1];
+		$image_quality = 80;
+		$result_filename = $save_full_path;
+		$original_src = $save_full_path;
 
-	$targ_w = $imageSize[0];
-	$targ_h = $imageSize[1];
-	$image_quality = 80;
-	$result_filename = $save_full_path;
+		if($mimeType == "image/png"){
+			$img_r = imagecreatefrompng($original_src);
+		}else{
+			$img_r = imagecreatefromjpeg($original_src);
+		}
 
-	$original_src = $save_full_path;
-	$img_r = imagecreatefrompng($original_src);
-	$dst_r = ImageCreateTrueColor($targ_w, $targ_h);
-
-	imagecopyresampled($dst_r,$img_r,0,0,0,0,
+		$dst_r = ImageCreateTrueColor($targ_w, $targ_h);
+		imagecopyresampled($dst_r,$img_r,0,0,0,0,
 		$targ_w,$targ_h,$targ_w,$targ_h);
+		imagejpeg($dst_r, $result_filename, $image_quality);
+		imagedestroy($dst_r);
 
-	// header('Content-type: image/jpeg');
-	imagejpeg($dst_r, $result_filename, $image_quality);
-	imagedestroy($dst_r);
-	?>
+	}
+	echo $result_filename;
+?>
