de7fc0cd7837aaa6cc3f73925ec03660e6342c60
kyh <kyhfan@gmail.com>
2016-06-20 16:51:09 +0900

    크론탭 로직 수정

--- child_refresh.php
+++ child_refresh.php
@@ -4,14 +4,16 @@
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
 
-	$query 	= "SELECT mb_child FROM activator_info WHERE mb_regdate <'".date("Y-m-d",strtotime ("-3 days"))."'";
+	$query 	= "SELECT mb_child FROM activator_info WHERE mb_regdate <'".date("Y-m-d",strtotime ("-3 days"))."' AND mb_share_cnt=0";
 	$result 	= mysqli_query($my_db, $query);
 
 	if ($result)
 	{
 		$data	= mysqli_fetch_array($result);
 
-		$query2 	= "UPDATE child_info SET ch_choice='N' WHERE idx='".$data['mb_child']."'";
+
+
+		$query2 	= "UPDATE child_info SET ch_choice='N' WHERE idx='".$data['mb_child']."' AND ch_choice<>'Y'";
 		$result2 	= mysqli_query($my_db, $query2);
 	}
 
