3f0eb71c17912bf035b6681d493b2886213244ae
kyh <kyhfan@gmail.com>
2016-06-20 14:26:41 +0900

    크론탭 파일 수정

--- child_refresh.php
+++ child_refresh.php
@@ -1,16 +1,17 @@
 <?
-	include_once "include/global.php"; 			//변수정보
-	include_once "include/function.php"; 		//함수정보
-	include_once "include/dbi.php"; 			//DB 연결정보
+	$my_db = new mysqli("localhost", "root", "7alslqjxkdlwld@%*)1590", "compassion_dream");
+	if (mysqli_connect_error()) {
+		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+	}
 
-	$query 	= "SELECT mb_child FROM ".$_gl['activator_info_table']." WHERE mb_regdate <'".date("Y-m-d",strtotime ("-3 days"))."'";
+	$query 	= "SELECT mb_child FROM activator_info WHERE mb_regdate <'".date("Y-m-d",strtotime ("-3 days"))."'";
 	$result 	= mysqli_query($my_db, $query);
 
 	if ($result)
 	{
 		$data	= mysqli_fetch_array($result);
 
-		$query 	= "UPDATE ".$_gl['child_info_table']." SET ch_choice='N' WHERE idx='".$data['mb_child']."'";
+		$query 	= "UPDATE child_info SET ch_choice='N' WHERE idx='".$data['mb_child']."'";
 		$result 	= mysqli_query($my_db, $query);
 	}
 
