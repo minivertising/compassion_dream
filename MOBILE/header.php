e89bd6ac5883308083c718fcac6ad914fa197a31
kyh <kyhfan@gmail.com>
2016-06-22 11:23:44 +0900

    모바일 헤더 파일 변경

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -2,14 +2,21 @@
 	include_once "../config.php";
 
 	$rs	= $_REQUEST['rs'];
-
-	$mb_query 	= "SELECT * FROM ".$_gl['activator_info_table']." WHERE mb_serial='".$rs."'";
-	$mb_result 	= mysqli_query($my_db, $mb_query);
-	$mb_data	= mysqli_fetch_array($mb_result);
-
-	$img_url		= str_replace("..","http://www.mnv.kr",$mb_data['mb_image']);
-
-
+	$ugu	= $_REQUEST['ugu'];
+	if (isset($rs))
+	{
+		if ($ugu	== "act")
+		{
+			$mb_query 	= "SELECT * FROM ".$_gl['activator_info_table']." WHERE mb_serial='".$rs."'";
+			$mb_result 	= mysqli_query($my_db, $mb_query);
+			$mb_data	= mysqli_fetch_array($mb_result);
+		}else{
+			$mb_query 	= "SELECT * FROM ".$_gl['follower_info_table']." WHERE mb_serial='".$rs."'";
+			$mb_result 	= mysqli_query($my_db, $mb_query);
+			$mb_data	= mysqli_fetch_array($mb_result);
+		}
+		$img_url		= str_replace("..","http://www.mnv.kr",$mb_data['mb_image']);
+	}
 ?>
 <!doctype html>
 <html>
@@ -18,8 +25,21 @@
 <meta name="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0, maximum-scale=1.0"/>
 <meta property="og:title" content="나의 어릴적 꿈을 소개합니다">
 <meta property="og:type" content="website" />
+<?
+	if (isset($rs))
+	{
+// PC, MOBILE 구분 ?
+?>
 <meta property="og:url" content="http://www.mnv.kr/MOBILE/lms_index.php?rs=<?=$mb_data['mb_serial']?>" />
 <meta property="og:image" content="<?=$img_url?>" />
+<?
+	}else{
+?>
+<meta property="og:url" content="http://www.mnv.kr/MOBILE/lms_index.php" />
+<meta property="og:image" content="" />
+<?
+	}
+?>
 <meta property="og:description" content="Compassion BLUE BATON CHALLENGE">
 <link rel="shortcut icon" type="image/x-icon" href="./img/icon/favicon.ico" />
 <title>모바일 메인</title>
