e73cbad00a795d9d1469675bd2be5151c9ac5f55
kyh <kyhfan@gmail.com>
2016-06-24 17:16:13 +0900

    마우스 커서 모양 변경

--- PC/index.php
+++ PC/index.php
@@ -144,7 +144,7 @@ $total_matching_cnt = 1000;
               <form id="ie_img_save" method="post" action="./ie_photo_upload2.php" enctype="multipart/form-data">
                 <label for="inputImage" title="Upload image file">
                   <input type="file" id="inputImage" class="sr-only" name="file" accept="image/*">
-                  <span title="Import image with Blob URLs"><img src="images/btn_select_pic.png" /></span>
+                  <span title="Import image with Blob URLs"><img src="images/btn_select_pic.png" style="cursor:pointer;"/></span>
                   <a href="#" onclick="preview_img();return false;"><img src="images/btn_preview.png" /></a>
                 </label>
               </form>
