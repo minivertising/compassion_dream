731d7f199ecbee90f5f24c1de92a5abb9de24caa
kyh <kyhfan@gmail.com>
2016-06-27 16:45:59 +0900

    1111

--- main_exec.php
+++ main_exec.php
@@ -69,10 +69,10 @@ switch ($_REQUEST['exec'])
 		if ($dupli_data)
 		{
 			$i = 0;
-			foreach($dupli_data as $key => $val)
+			foreach($dupli_data['mb_child'] as $key => $val)
 			{
 				// 이벤트 참여한적이 있을 경우
-				$ch_query 	= "SELECT * FROM ".$_gl['child_info_table']." WHERE idx='".$val[$i]."'";
+				$ch_query 	= "SELECT * FROM ".$_gl['child_info_table']." WHERE idx='".$val."'";
 				$ch_result 	= mysqli_query($my_db, $ch_query);
 				$ch_data		= mysqli_fetch_array($ch_result);
 				$check_choice	= false;
