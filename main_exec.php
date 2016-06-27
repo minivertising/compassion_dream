487b57ab081efe793b5842a0992822bef2ff4948
kyh <kyhfan@gmail.com>
2016-06-27 16:39:51 +0900

    1111

--- main_exec.php
+++ main_exec.php
@@ -68,10 +68,11 @@ switch ($_REQUEST['exec'])
 		$mb_serial	= create_serial("activator", null);
 		if ($dupli_data)
 		{
+			$i = 0;
 			foreach($dupli_data as $key => $val)
 			{
 				// 이벤트 참여한적이 있을 경우
-				$ch_query 	= "SELECT * FROM ".$_gl['child_info_table']." WHERE idx='".$val."'";
+				$ch_query 	= "SELECT * FROM ".$_gl['child_info_table']." WHERE idx='".$dupli_data[$i]."'";
 				$ch_result 	= mysqli_query($my_db, $ch_query);
 				$ch_data		= mysqli_fetch_array($ch_result);
 				$check_choice	= false;
@@ -84,6 +85,7 @@ switch ($_REQUEST['exec'])
 					print_r('111');
 					break;
 				}
+				$i++;
 			}
 
 			// 이벤트 참여한적이 있을 경우
