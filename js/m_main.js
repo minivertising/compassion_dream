e754da11284832fa3f22c1801212954cb2ce911e
ojoonwoo <jw.o@minivertising.kr>
2016-06-29 10:31:07 +0900

    모바일 팝업 페이드인 수정

--- js/m_main.js
+++ js/m_main.js
@@ -1,8 +1,7 @@
 function open_pop(param)
 {
 	// $('.preview > img').css('width', 'max-width');
-	// alert("op_pp");
-	$.colorbox({innerWidth:"100%",innerHeight: "70%", initialWidth:"95%", initialHeight: "70%", inline:true, opacity:"0.9", scrolling:true, reposition: false, closeButton:false, overlayClose: false, open:true, speed:0, transition: "fade", fadeOut: 300, href:"#"+param, onComplete: function(){
+	$.colorbox({innerWidth:"100%",innerHeight: "70%", initialWidth:"95%", initialHeight: "70%", inline:true, opacity:"0.9", scrolling:true, reposition: false, closeButton:false, overlayClose: false, open:true, /*speed:0,*/ transition: "fade", fadeOut: 300, href:"#"+param, onComplete: function(){
 		$("#cboxContent").css("background","none");
 		$("#cboxContent").css("z-index","99999");
 		$('#cboxWrapper').css('backgroundColor', "");
