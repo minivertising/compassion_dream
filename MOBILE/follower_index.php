da672e63dbcad1a2c53060b5dd14b231c570ea71
kyh <kyhfan@gmail.com>
2016-07-05 20:21:51 +0900

    팔로워 수정

--- MOBILE/follower_index.php
+++ MOBILE/follower_index.php
@@ -201,26 +201,21 @@
   </div>
 </div>
 
-<div id="f_matching_share_page" class="wrap_page share_match_child" style="display:none;">
+<div id="matching_share_page" class="wrap_page share_match_child" style="display:none;">
   <div class="inner">
     <div class="block_content">
       <div class="title">
         당신의 도움이 필요한 어린이는<br>
-        '<span id="m_rs_ch_name">아비가일 마아 야아 암퐁</span>'입니다
+        '<span id="m_rs_ch_name"><?=$ch_data['ch_nick']?></span>'입니다
       </div>
       <div class="block_child">
         <div class="inner_block_child clearfix">
           <div class="child_pic"><img src="images/ex_child.png" id="f_matching_child_pic" /></div>
           <div class="child_text">
-            <h2>"저도 <span id="m_rs_job">선생님을</span> 꿈꿀 수 있을까요?"</h2>
+            <h2>"저도 <span id="m_rs_job"><?=$mb_data['mb_job']?>을</span> 꿈꿀 수 있을까요?"</h2>
             <div class="bg_line">
               <p>
-              안녕하세요 <br>
-              저는  필리핀에 살고 있는 기타에요<br>
-              어린 시절에  선생님이 꿈이 셨군요<br>
-              저도 언젠가는 그렇게 멋진 꿈을 꾸고 싶어요!
-              어린 시절에  선생님이 꿈이 셨군요<br>
-              어린 시절에  선생님이 꿈이 셨군요<br>
+             <?=$ch_data['ch_desc']?>
               </p>
             </div>
           </div>
@@ -228,15 +223,12 @@
       </div>
       <div class="block_txt">
         SNS에 공유하셔서<br>
-        <span id="m_rs_ch_name3">기타</span>의 후원자를 찾아주세요
+        <span id="m_rs_ch_name3"><?=$ch_data['ch_nick']?></span>의 후원자를 찾아주세요
       </div>
       <div class="block_btn sns">
-        <!-- <a href="#" onclick="sns_share('fb','act');return false;"><img src="images/sns_f.png" /></a>
-        <a href="#" onclick="sns_share('kt','act');return false;"><img src="images/sns_kt.png" /></a>
-        <a href="#" onclick="sns_share('ks','act');return false;"><img src="images/sns_ks.png" /></a> -->
-        <a href="#" onclick="go_share('fb','act','matching_share_page');return false;"><img src="images/sns_f.png" /></a>
-        <a href="#" onclick="go_share('kt','act','matching_share_page');return false;"><img src="images/sns_kt.png" /></a>
-        <a href="#" onclick="go_share('ks','act','matching_share_page');return false;"><img src="images/sns_ks.png" /></a>
+        <a href="#" onclick="go_share('fb','fol','matching_share_page');return false;"><img src="images/sns_f.png" /></a>
+        <a href="#" onclick="go_share('kt','fol','matching_share_page');return false;"><img src="images/sns_kt.png" /></a>
+        <a href="#" onclick="go_share('ks','fol','matching_share_page');return false;"><img src="images/sns_ks.png" /></a>
       </div>
       <div class="block_btn howtotag">
         <a href="#"  onclick="open_pop('exam_share_popup');return false;" class="clearfix">
@@ -251,7 +243,7 @@
   </div>
 </div>
 
-<div  id="f_no_matching_page" class="wrap_page share_match_child" style="display:none;">
+<div  id="no_matching_page" class="wrap_page share_match_child" style="display:none;">
   <div class="inner">
     <div class="block_content">
       <div class="title compassion">
@@ -289,6 +281,43 @@
     </div>
   </div>
 </div>
+
+<!-- 공유버튼 클릭시 나오는 예시 페이지 -->
+<div id="sns_exam_page" class="wrap_page sns_exam" style="display:none;">
+  <div class="inner">
+    <div class="block_content">
+      <div class="img"> 
+        <img src="images/sns_share.jpg" class="bg">
+      </div>
+      <div class="btn_block">
+        <a href="#" id="go_share_func"><img src="images/btn_keepgoing.png"></a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 공유버튼 클릭시 나오는 예시 페이지 -->
+
+<!-- 공유 완료 페이지 -->
+<div id="thanks_page" class="wrap_page share_match_child" style="display:none">
+  <div class="inner">
+    <div class="block_content">
+      <div class="title">
+        참여해주셔서 감사합니다<br>
+        <span id="thx_ch_name">'<?=$ch_data['ch_nick']?>'</span>이<br>
+        꿈을 꿀 수 있도록 끝까지 함께 응원해주세요
+      </div>
+      <div class="block_child re">
+        <div class="inner_block_child clearfix">
+          <div class="child_pic re"><img src="<?=$ch_data['ch_full_img_url']?>" id="thx_ch_img" /></div>
+        </div>
+      </div>
+      <div class="block_btn ok">
+        <a href="#" onclick="location.reload();"><img src="images/btn_ok.png" /></a>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 공유 완료 페이지 -->
 
 <?
         include_once "./popup_div.php";
@@ -445,18 +474,18 @@ function f_dream_next()
                 //$("#contents_div").show();
                 if (rs_ch[0] == "Y")
                 {
-                    $("#f_matching_child_pic").attr("src","<?=$ch_data['ch_top_img_url']?>");
+                    $("#matching_child_pic").attr("src","<?=$ch_data['ch_top_img_url']?>");
 					setTimeout(function(){
 	                $("#loading_div").fadeOut('fast',function(){
 <?
 	if ($ch_data['ch_choice'] == "Y")
 	{
 ?>
-						$("#f_no_matching_page").fadeIn("fast");
+						$("#no_matching_page").fadeIn("fast");
 <?
 	}else{
 ?>
-						$("#f_matching_share_page").fadeIn("fast");
+						$("#matching_share_page").fadeIn("fast");
 <?
 	}
 ?>
@@ -465,7 +494,7 @@ function f_dream_next()
                 }else if (rs_ch[0] == "N"){
 					setTimeout(function(){
 	                $("#loading_div").fadeOut('fast',function(){
-						$("#f_no_matching_page").fadeIn("fast");
+						$("#no_matching_page").fadeIn("fast");
 					});
 					},1500);
                 }else {
