bf41f559f066939c2ceeae28f87bfc9aadc9658a
kyh <kyhfan@gmail.com>
2016-07-06 20:45:12 +0900

    모바일 팔로워 페이지 수정

--- MOBILE/follower_index.php
+++ MOBILE/follower_index.php
@@ -118,36 +118,30 @@
   <div id="page_div6" class="wrap_page share_match_child" style="display:none;">
     <div class="inner">
       <div class="block_content">
-        <div class="title compassion">
-        "얘들아, 내꿈꿔~!"
+        <div class="title">
+          "<span><?=$ch_data['ch_nick']?></span>아<br>
+          내꿈꿔!"	
         </div>
-        <div class="sub_txt">
-        컴패션 소개와 함께 어린 시절 사진을 sns에 공유하면<br>
-        꿈을 잃어버린 어린이들을 도와줄 수 있습니다
-        </div>
-        <div class="img_com">
-          <div class="img_child">
-            <div class="inner_img_child clearfix">
-              <div class="one"><img src="images/ex_child.png" /></div>
-              <div class="one"><img src="images/ex_child.png" /></div>
-              <div class="one"><img src="images/ex_child.png" /></div>
-            </div>
+        <div class="block_child re">
+          <div class="inner_block_child clearfix">
+            <div class="child_pic re"><img src="<?=$ch_data['ch_full_img_url']?>" /></div>
           </div>
-          <img src="images/bg_share_com.png" class="bg" />
         </div>
-        <div class="block_btn spon">
-          <div class="bt"><a href="#"><img src="images/btn_spon.png" /></a></div>
-          <div class="txt">1:1후원으로 기타의 꿈을 현실로 만들어주세요!</div>
-        </div>
-        <div class="block_btn cheer">
-          <div class="bt"><a href="#" onclick="f_show_dream_sel();return false;"><img src="images/btn_cheer.png" /></a></div>
-          <div class="txt">1:1 후원이 어려울 경우<br> SNS에 어릴적 사진을 공유하고 함께 응원해주세요</div>
-        </div>
-        <div class="friends_pic">
-          <div class="inner_friends_pic clearfix">
-            <div class="img"><img src="images/ex_friend.png" /></div>
-            <div class="txt">어린 시절  꿈과 사진을 등록하고<br> SNS공유하면 응원 완료!</div>
+        <div class="more_child_list">
+          <div class="inner_more_child_list clearfix">
+            <div class="one"><img src="images/ex_child.png" /></div>
+            <div class="one"><img src="images/ex_child.png" /></div>
+            <div class="one"><img src="images/ex_child.png" /></div>
+            <div class="one"><img src="images/ex_child.png" /></div>
           </div>
+          <div class="txt">
+          컴패션에는 꿈이 필요한 어린이들이 많이 있어요<br>
+          당신의 어린 시절 꿈과 사진을 공유하셔서<br> 
+          어린이의 꿈을 응원해주세요
+          </div>
+        </div>
+        <div class="block_btn re">
+          <a href="index.php"><img src="images/btn_apply_re.png" /></a>
         </div>
       </div>
     </div>
