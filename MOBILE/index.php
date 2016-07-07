368de806d3284480df47115e45c643540561984c
ojoonwoo <jw.o@minivertising.kr>
2016-07-07 14:19:32 +0900

    aa

--- MOBILE/index.php
+++ MOBILE/index.php
@@ -93,17 +93,7 @@
 	</div>
 	<div class="people_list">
 		<div class="inner_people_list clearfix">
-<?
-	$slider_query = "SELECT * FROM ".$_gl['activator_info_table']." order by idx DESC limit 20";
-	$slider_res = mysqli_query($my_db, $slider_query);
-
-	while ($slider_data = @mysqli_fetch_array($slider_res))
-	{
-?>
-					<div class="one"><img src="<?=$slider_data['mb_image']?>" /></div>
-<?
-	}
-?>
+					<div class="one"><img src="images/ex_list.png" /></div>
 					<div class="one"><img src="images/ex_list.png" /></div>
 					<div class="one"><img src="images/ex_list.png" /></div>
 					<div class="one"><img src="images/ex_list.png" /></div>
