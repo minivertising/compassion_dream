e0d4c6902dd58dd9b153c6ea7a81d99f74445ebf
kyh <kyhfan@gmail.com>
2016-07-08 20:15:59 +0900

    11122

--- main_exec.php
+++ main_exec.php
@@ -270,9 +270,29 @@ switch ($_REQUEST['exec'])
 			die("파일을 지정한 디렉토리에 업로드하는데 실패했습니다.");
 		}else{
 			// 이미지 로테이트 
-
-			@$image = imagecreatefromjpeg($dest_url) or die('Error opening file '.$dest_url);
-
+			$ori_images	= str_replace("..",".",$dest_url);
+			@$image = imagecreatefromjpeg($ori_images) or die('Error opening file '.$ori_images);
+			
+			$exif = exif_read_data($ori_images);
+			 
+			if(!empty($exif['Orientation'])) {
+				switch($exif['Orientation']) {
+					case 8:
+						$image = imagerotate($image,90,0);
+						break;
+					case 3:
+						$image = imagerotate($image,180,0);
+						break;
+					case 6:
+						$image = imagerotate($image,-90,0);
+						break;
+				}
+			}
+			header('Content-type: image/jpeg');
+			 
+			imagejpeg($image);
+			 
+			imagedestroy($image);
 
 			$mb_serial	= create_serial("follower",$rs);
 			$dest_url	= ".".$dest_url;
