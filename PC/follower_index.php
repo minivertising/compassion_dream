c2d3773aa583f8e235598cd5b86731f9dc65fbb4
kyh <kyhfan@gmail.com>
2016-07-13 13:00:38 +0900

    1111

--- PC/follower_index.php
+++ PC/follower_index.php
@@ -1450,5 +1450,14 @@ function f_dream_next()
             }
         });
     }
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80730015-1', 'auto');
+  ga('send', 'pageview');
+
 </script>
 <!-- <script src="../lib/Cropper/js/main.js"></script> -->
